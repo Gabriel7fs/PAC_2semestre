c7d97b2d20386c2e9729c8f84459c1d23a01838b
Gabriel Furquim da Silva <88550409+Gabriel7fs@users.noreply.github.com>
2021-10-13 20:42:59 -0300

    banco de dados

---
+++ Jogo PPT/config.php
@@ -0,0 +1,19 @@
+<?php
+
+    $dbHost='localhost';
+    $dbUser= 'root';
+    $dbPassword='Xj6m3@rC';
+    $dbName='formulario_jogo';
+
+    $conexao = new mysqli($dbHost, $dbUser, $dbPassword, $dbName);
+
+    if($conexao->connect_errno){
+
+        echo "Erro";
+    }
+    else{
+
+        echo "Conexao efetuada"
+    }
+
+?>
